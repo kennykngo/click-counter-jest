2429d15d228114b76fe8691e738a99bd051e005b
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-12 13:53:35 -0800

    created a findByTestAttr(), which takes in wrapper and val to output Enzyme syntax

--- src/App.test.js
+++ src/App.test.js
@@ -18,22 +18,23 @@ Enzyme.configure({ adapter: new Adapter() });
  */
 
 const setup = () => shallow(<App />);
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
 test("renders without error", () => {
   const wrapper = setup();
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const appComponent = findByTestAttr(wrapper, "component-app");
 
   // if the ASSERTION OR the expect method to find the appComponent to exist, then PASS
   expect(appComponent.length).toBe(1);
 });
 test("renders button", () => {
   const wrapper = setup();
-  const button = wrapper.find("[data-test='increment-button']");
+  const button = findByTestAttr(wrapper, "increment-button");
   expect(button.length).toBe(1);
 });
 test("renders counter display", () => {
   const wrapper = setup();
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
 test("counter starts at 0", () => {});
