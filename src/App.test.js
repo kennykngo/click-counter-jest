c9a5256e4c6ac74450640426ee50ba9a5ee2525c
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-12 13:04:16 -0800

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+// import { shallow } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// npm install --save-dev @wojtekmaj/enzyme-adapter-react-17
+
+import App from "./App";
+
+// set up enzyme's react adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+// test("renders learn react link", () => {
+//   // shallow function takes jsx and returns a shallow wrapper
+
+//   const wrapper = shallow(<App />);
+//   expect(wrapper).toBeTruthy();
+// });
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+
+  // if the ASSERTION OR the expect method to find the appComponent to exist, then PASS
+  expect(appComponent.length).toBe(1);
 });
+test("renders button", () => {});
+test("renders counter display", () => {});
+test("counter starts at 0", () => {});
+test("clicking on button increments counter display", () => {});
