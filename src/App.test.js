3d95f001308efd48aaaad1b37eb029669cf4330b
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-12 13:31:27 -0800

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -23,7 +23,15 @@ test("renders without error", () => {
   // if the ASSERTION OR the expect method to find the appComponent to exist, then PASS
   expect(appComponent.length).toBe(1);
 });
-test("renders button", () => {});
-test("renders counter display", () => {});
+test("renders button", () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
+test("renders counter display", () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
 test("counter starts at 0", () => {});
 test("clicking on button increments counter display", () => {});
