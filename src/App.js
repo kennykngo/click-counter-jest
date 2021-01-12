868634668c09d816e65751cb71f9cde90be21dd8
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-12 14:07:16 -0800

    added a test function for 'counter starts at 0'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "./logo.svg";
 
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div data-test="component-app" className="App">
-      <h1 data-test="counter-display">The counter is currently</h1>
+      <h1 data-test="counter-display">
+        The counter is currently &nbsp; <span data-test="count">{count}</span>
+      </h1>
       <button data-test="increment-button">Increment counter</button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -32,10 +32,16 @@ test("renders button", () => {
   const button = findByTestAttr(wrapper, "increment-button");
   expect(button.length).toBe(1);
 });
+
 test("renders counter display", () => {
   const wrapper = setup();
   const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
-test("counter starts at 0", () => {});
+
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
+});
 test("clicking on button increments counter display", () => {});
